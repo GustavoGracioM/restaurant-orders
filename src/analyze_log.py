993e00bf522a98c2c5e948153001d37183018527
GustavoGracioM <gustavogracio@hotmail.com>
2022-12-15 15:55:34 -0300

    refatora funções de analyze_log

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -16,32 +16,21 @@ def read_file_csv(path_to_file):
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
 
-def formatter(list_request: list):
-    result_list = {}
-    for name, prato, day in list_request:
-        if name not in result_list:
-            result_list[name] = {}
-        if day not in result_list[name]:
-            result_list[name][day] = {}
-        if prato not in result_list[name][day]:
-            result_list[name][day][prato] = 0
-        result_list[name][day][prato] += 1
-
-    return result_list
-
-
-def product_set(list_request: list, name: str):
-    set_request = {}
-    for day in list_request[name]:
-        for product in list_request[name][day]:
-            if product not in set_request:
-                set_request[product] = 0
-            set_request[product] += list_request[name][day][product]
-
-    return set_request
+def infos_client(list_request, name_person):
+    products = {}
+    days = {}
+    for name, product, day in list_request:
+        if name == name_person:
+            if product not in products:
+                products[product] = 0
+            products[product] += 1
+            if day not in days:
+                days[day] = 0
+            days[day] += 1
+    return [products, days]
 
 
-def infos_client(list_request: list):
+def infos_requests(list_request: list):
     requests = set()
     days = set()
     for _, request, day in list_request:
@@ -52,36 +41,14 @@ def infos_client(list_request: list):
     return [requests, days]
 
 
-def days_set(list: list, name):
-    days = set()
-    for day in list[name]:
-        if day not in days:
-            days.add(day)
-    return days
-
-
-def max_request(formatedet, name):
-    request = product_set(formatedet, name)
-    max_quantity = 0
-    name_product = ""
-    for r in request:
-        if max_quantity < request[r]:
-            max_quantity = request[r]
-            name_product = r
-    return name_product
-
-
-def get_quatity(formatedet, name, product):
-    return product_set(formatedet, name)[product]
-
-
-def get_not_product(formatedet, products, name):
-    result = set(product_set(formatedet, name).keys())
-    return result.symmetric_difference(products)
-
-
-def get_not_day(formatedet, days, name):
-    return days_set(formatedet, name).symmetric_difference(days)
+def get_max(list_request: list):
+    max_value = 0
+    max_name = ""
+    for key in list_request:
+        if max_value < list_request[key]:
+            max_name = key
+            max_value = list_request[key]
+    return (max_name, max_value)
 
 
 def write_analyze_lg(logs):
@@ -93,10 +60,15 @@ def write_analyze_lg(logs):
 def analyze_log(path_to_file):
     logs = []
     request = read_file_csv(path_to_file)
-    request_formated = formatter(request)
-    pratos, days = infos_client(request)
-    logs.append(max_request(request_formated, "maria"))
-    logs.append(get_quatity(request_formated, "arnaldo", "hamburguer"))
-    logs.append(get_not_product(request_formated, pratos, "joao"))
-    logs.append(get_not_day(request_formated, days, "joao"))
+    products, days = infos_requests(request)
+    product_maria, _ = infos_client(request, "maria")
+    product_arnaldo, _ = infos_client(request, "arnaldo")
+    product_joao, days_joao = infos_client(request, "joao")
+    logs.append(get_max(product_maria)[0])
+    logs.append(product_arnaldo['hamburguer'])
+    logs.append(set(product_joao.keys()).symmetric_difference(products))
+    logs.append(set(days_joao.keys()).symmetric_difference(days))
     write_analyze_lg(logs)
+
+
+print(analyze_log("data/orders_1.csv"))
